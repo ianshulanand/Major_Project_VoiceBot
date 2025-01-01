e9d823c8135443f84712d30348f84ad21b075114
Anshul Anand <88222186+ianshulanand@users.noreply.github.com>
2025-01-01 15:47:40 +0530

    Delete bot directory

--- bot/jokes.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-def get_joke():
-    response = requests.get("https://official-joke-api.appspot.com/random_joke")
-    joke = response.json()
-    return f"{joke['setup']} - {joke['punchline']}"
--- bot/news.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-def get_news(api_key):
-    url = f"https://newsapi.org/v2/top-headlines?country=us&apiKey={api_key}"
-    response = requests.get(url)
-    news = response.json()
-    articles = news['articles'][:5]
-    headlines = [f"{article['title']} - {article['source']['name']}" for article in articles]
-    return "\n".join(headlines)
